83d9250af85365bf4ce0c16cdd004ae3bd5b2e0e
maghsood <maghsood@localhost.localdomain>
2018-07-29 14:45:41 +0200

    Expense is there

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from web.models import Expense
 from django.contrib import admin
 
+
+admin.site.register(Expense)
 # Register your models here.
--- web/models.py
+++ web/models.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    time = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    time = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
